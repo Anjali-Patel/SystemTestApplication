fdb26e0f4b375805128d51562ba7372ff02ba4f0
Anjali-Patel <anjalics14.academic@gmail.com>
2020-09-27 11:24:16 +0530

    second changes

--- app/src/main/java/com/factor/systemtestapplication/YouTubeConfig.java
+++ app/src/main/java/com/factor/systemtestapplication/YouTubeConfig.java
@@ -1,7 +1,8 @@
 package com.factor.systemtestapplication;
 
-public class YouTubeConfig {
-    private YouTubeConfig() {
+ public class YouTubeConfig {
+
+     private   YouTubeConfig() {
     }
     public static final String YOUTUBE_API_KEY = "AIzaSyCc5XgS_c_81UlNDgc_6Vw1HLHNccCkOxQ";
 }
